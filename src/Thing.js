1a3a69c897be33ac858d66ad46fd8ff030aa1fd3
Ethan Hansen <hanseneg@hotmail.com>
2021-01-13 14:24:07 -0700

    commit9

--- src/Thing.js
+++ src/Thing.js
@@ -4,10 +4,9 @@ function Thing(props) {
     return(
         <div>
             <header>{props.title}</header>
-            <img src={props.imgUrl}/>
+            <img src={props.imgUrl} alt=''/>
             <p>{props.description}</p>
             <button onClick={() => props.deleteButton(props.id)}>Delete</button>
-            <button>Edit</button>
         </div>
     )
 }
